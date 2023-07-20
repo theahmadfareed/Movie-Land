403b86a48677c327f873a412e1b3e00eb29f2d9e
ahmad <ha5403905@gmail.com>
2023-07-20 11:37:23 +0500

    .env added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
